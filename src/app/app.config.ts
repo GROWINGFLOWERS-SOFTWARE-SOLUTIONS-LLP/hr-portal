a307dc4f217f422b115f0ac612a632f849a6b348
Shruti Dhawde <shruti.dhawde@growingflowers-solutions.com>
2025-06-03 15:08:21 +0530

    meetings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
@@ -9,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
     providePrimeNG({
       theme: {
         preset: Aura
--- src/app/features/hr/meetings/meetings.component.ts
+++ src/app/features/hr/meetings/meetings.component.ts
@@ -1,12 +1,78 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextarea } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+ 
 @Component({
   selector: 'app-meetings',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    DropdownModule,
+    CalendarModule,
+    InputTextModule,
+    InputTextarea,
+    ButtonModule,
+    TableModule
+  ],
   templateUrl: './meetings.component.html',
   styleUrl: './meetings.component.css'
 })
 export class MeetingsComponent {
-
+ 
+  hrNames = [
+  { label: 'Shruti', value: 'Shruti' },
+  { label: 'Priya Patel', value: 'Priya Patel' }
+];
+ 
+  meeting = {
+    employeeName: null,
+    hrName: null,
+    date: null,
+    topic: '',
+    conclusion: ''
+  };
+ 
+  selectedIndex: number | null = null;
+ 
+  employeeOptions = [
+    { name: 'Ankita Sharma', code: 'E1' },
+    { name: 'Rahul Jain', code: 'E2' },
+    { name: 'Sneha Patil', code: 'E3' }
+  ];
+ 
+  meetingsList: any[] = [];
+ 
+  onSave() {
+    if (this.selectedIndex !== null) {
+      this.meetingsList[this.selectedIndex] = { ...this.meeting };
+      this.selectedIndex = null;
+    } else {
+      this.meetingsList.push({ ...this.meeting });
+    }
+    this.onClear();
+  }
+ 
+  onEdit(index: number) {
+    this.selectedIndex = index;
+    this.meeting = { ...this.meetingsList[index] };
+  }
+ 
+  onClear() {
+    this.meeting = {
+      employeeName: null,
+      hrName: null,
+      date: null,
+      topic: '',
+      conclusion: ''
+    };
+    this.selectedIndex = null;
+  }
 }
+ 
