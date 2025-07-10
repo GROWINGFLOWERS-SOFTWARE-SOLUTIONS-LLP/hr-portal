6ccf5d23775491acb1eff9caf6c5cb0ba2cfc60e
paamrutkar <prasad.amrutkar@growingflowers-solutions.com>
2025-07-10 11:50:41 +0530

    My-hr Changes

--- src/app/features/hr/my-hr/my-hr.component.ts
+++ src/app/features/hr/my-hr/my-hr.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-my-hr',
   standalone: true,
-  imports: [],
+  imports: [ CardModule, ButtonModule,],
   templateUrl: './my-hr.component.html',
   styleUrl: './my-hr.component.css'
 })
